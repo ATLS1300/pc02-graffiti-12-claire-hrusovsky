9a80217feaee1cde0d38edbe15bfb70d54a26307
clairehrusovsky <70406426+clairehrusovsky@users.noreply.github.com>
2020-09-04 12:39:42 -0600

    Add files via upload

---
+++ PC02_Graffiti_Hrusovsky_Hrusovsky.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 11:39:02 2020
+
+@author: ClaireHrusovsky
+"""
+
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+Turtle starter code
+ATLS 1300
+Author: Dr. Z
+Author: YOUR NAME
+May 29, 2020
+'''
+
+from turtle import * #import the library of commands that you'd like to use
+
+colormode(255)
+
+# Create a panel to draw on. 
+panel = Screen()
+w = 750 # width of panel
+h = 750 # height of panel
+panel.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
+#You can experiment by making it the size of your screen or super tiny!
+
+# Create a colorful background and add Bezos image to it
+image = "Bezos.gif"
+panel.bgcolor("lightsteelblue1")
+panel.bgpic(image)
+
+t = Turtle()
+t.pensize(10)
+
+circle(100) 
+curve(50)
+
+up()
+setheading(90)
+
+down()
+
+
+up()
+
+setheading(90)
+
+
+
+forward (20)
+
+setheading(90)
+
+
+
+circle(90)
+
+down()
+
+setheading(90)
+
+setheading(0)
+
+circle(90)
+
+up()
+
+setheading(90)
+
+forward(20)
+
+setheading(0)
+
+circle(80)
+
+down()
+
+circle(80)
+
+up()
+
+setheading(90)
+
+forward(20)
+
+down()
+
+setheading(0)
+
+circle(70)
+
+up()
+
+setheading(90)
+
+forward(20)
+
+setheading(0)
+
+down()
+
+circle(60)
+
+up()
+
+setheading(90)
+
+forward(20)
+
+setheading(0)
+
+down()
+
+circle(50)
+
+up()
+
+setheading(90)
+
+forward(20)
+
+setheading(0)
+
+down()
+
+circle(40)
+
+up()
+
+setheading(90)
+
+forward(20)
+
+setheading(0)
+
+down()
+
+circle(30)
+
+pensize(20)
+
+circle(30)
+
+up()
+
+setheading(270)
+
+forward(400)
+
+setheading(90)
+
+forward(100)
+
+down()
+
+forward(50)
+
+forward(20)
+
+forward(10)
+
+forward(10)
+
+setheading(0)
+
+forward(100)
+
+setheading(27)
+
+forward(100)
+
+up()
+
+setheading (25)
+
+0
+Out[76]: 0
+
+setheading(230)
+
+forward(100)
+
+setheading (300)
+
+setheading(270)
+
+forward(100)
+
+setheading(90)
+
+forward(50)
+
+forward(10)
+
+setheading(45)
+
+setheading(180)
+
+down()
+
+pensize(20)
+
+forward(40)
+
+forward(50)
+
+forward(10)
+
+forward(5)
+
+forward(5)
+
+forward(5)
+
+setheading(0)
+
+forward(115)
+
+setheading(90)
+
+forward(50)
+
+setheading(180)
+
+forward(50)
+
+forward(50)
+
+forward(10)
+
+forward(5)
+
+
