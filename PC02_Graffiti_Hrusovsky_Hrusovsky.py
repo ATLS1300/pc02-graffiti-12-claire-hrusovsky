079ac08fd347f9abd436cc1b35e526fb90a1cf25
clairehrusovsky <70406426+clairehrusovsky@users.noreply.github.com>
2020-10-04 15:34:13 -0600

    Update PC02_Graffiti_Hrusovsky_Hrusovsky.py
    Code review added October 4, 2020 - Claire Hrusovsky

--- PC02_Graffiti_Hrusovsky_Hrusovsky.py
+++ PC02_Graffiti_Hrusovsky_Hrusovsky.py
@@ -4,234 +4,88 @@
 Created on Fri Sep  4 11:39:02 2020
 
 @author: ClaireHrusovsky
+
+A graffit art piece that takes Jeff Bezos face and creates a target like circle pattered around his face as well as a geometric square around his pointed hands.
+# I really enjoyed my code on this project. It was when we were first learning everything so it was pretty extensive and step by step. I added a loop as well as organized the code to be easier understood. The overal look for the code is funny and has the potential to be such a fun creative coder piece. 
 """
 
-#!/usr/bin/env python
-# coding: utf-8
+from turtle import 
 
-'''
-Turtle starter code
-ATLS 1300
-Author: Dr. Z
-Author: YOUR NAME
-May 29, 2020
-'''
-
-from turtle import * #import the library of commands that you'd like to use
-
-colormode(255)
+#Removed colormode
 
 # Create a panel to draw on. 
 panel = Screen()
 w = 750 # width of panel
 h = 750 # height of panel
-panel.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
-#You can experiment by making it the size of your screen or super tiny!
+panel.setup(width=w, height=h) 
+
 
 # Create a colorful background and add Bezos image to it
 image = "Bezos.gif"
 panel.bgcolor("lightsteelblue1")
 panel.bgpic(image)
 
-t = Turtle()
-t.pensize(10)
+#defined turtle and decreased the pensize from 10 to 1 to ensure the sprial was defined. 
+t = Turtle() 
+t.pensize(1)
 
+#begin target like shapes around Bezos' face.
 circle(100) 
 curve(50)
 
 up()
 setheading(90)
-
 down()
 
-
 up()
-
 setheading(90)
-
-
-
 forward (20)
-
-setheading(90)
-
-
-
 circle(90)
-
 down()
 
 setheading(90)
-
 setheading(0)
-
 circle(90)
 
 up()
-
 setheading(90)
-
 forward(20)
-
 setheading(0)
-
 circle(80)
-
 down()
 
 circle(80)
-
 up()
-
 setheading(90)
-
 forward(20)
-
 down()
 
 setheading(0)
-
 circle(70)
-
 up()
-
 setheading(90)
-
 forward(20)
-
 setheading(0)
-
 down()
 
 circle(60)
-
 up()
-
 setheading(90)
-
 forward(20)
-
 setheading(0)
-
 down()
-
 circle(50)
 
 up()
-
 setheading(90)
-
 forward(20)
-
 setheading(0)
-
 down()
-
 circle(40)
 
-up()
-
-setheading(90)
-
-forward(20)
-
-setheading(0)
-
-down()
-
-circle(30)
-
-pensize(20)
-
-circle(30)
-
-up()
-
-setheading(270)
-
-forward(400)
-
-setheading(90)
-
-forward(100)
-
-down()
-
-forward(50)
-
-forward(20)
-
-forward(10)
-
-forward(10)
-
-setheading(0)
-
-forward(100)
-
-setheading(27)
-
-forward(100)
-
-up()
-
-setheading (25)
-
-0
-Out[76]: 0
-
-setheading(230)
-
-forward(100)
-
-setheading (300)
-
-setheading(270)
-
-forward(100)
-
-setheading(90)
-
-forward(50)
-
-forward(10)
-
-setheading(45)
-
-setheading(180)
-
-down()
-
-pensize(20)
-
-forward(40)
-
-forward(50)
-
-forward(10)
-
-forward(5)
-
-forward(5)
-
-forward(5)
-
-setheading(0)
-
-forward(115)
-
-setheading(90)
-
-forward(50)
-
-setheading(180)
-
-forward(50)
-
-forward(50)
-
-forward(10)
-
-forward(5)
-
-
+#creating a spiral action for the turtle instead of indivial squares around his hand. This will give the same look with more concise code. I know this was the complexity I was looking for.
+side = 200
+for i in range(100):
+   t.forward(side)
+   t.right(90) #Exterior angle of a square is 90 degree
+   side = side - 2
